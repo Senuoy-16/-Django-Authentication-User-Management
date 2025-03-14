b7b4c054a68faeaf596f035fa71f93cc757a98f5
Younes <youn6197@gmail.com>
2025-03-14 13:56:27 +0000

    seconde commit

--- website/settings.py
+++ website/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xx-ctrxbv6!vno8_y08s_p2d#fl5+*@=6*ld%%f+)fu=(%4ftr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
